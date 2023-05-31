7284e236a21eba87ceb57928fee6540df77d858e
Alin Cruceat <alyn_509@yahoo.com>
2023-05-31 15:09:13 +0300

    update path failing contracts 2

--- contracts/wegld-swap/tests/wegld_swap_scenario_rs_test.rs
+++ contracts/wegld-swap/tests/wegld_swap_scenario_rs_test.rs
@@ -2,7 +2,7 @@ use multiversx_sc_scenario::*;
 
 fn world() -> ScenarioWorld {
     let mut blockchain = ScenarioWorld::new();
-    blockchain.set_current_dir_from_workspace("contracts/core/wegld-swap");
+    blockchain.set_current_dir_from_workspace("contracts/wegld-swap");
 
     blockchain.register_contract(
         "file:output/multiversx-wegld-swap-sc.wasm",
