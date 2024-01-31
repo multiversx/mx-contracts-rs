039b31d0bcdbd859a7f5a9192ecfe8edf558e8d2
Dorin Marian Iancu <dorin.iancu@multiversx.com>
2024-01-31 11:54:56 +0200

    ignore instead of comment test

--- contracts/multisig/tests/multisig_scenario_go_test.rs
+++ contracts/multisig/tests/multisig_scenario_go_test.rs
@@ -54,22 +54,23 @@ fn deploy_duplicate_bm_go() {
     world().run("scenarios/deploy_duplicate_bm.scen.json");
 }
 
-// #[test]
-// #[ignore = "system SC not yet implemented"]
-// fn interactor_nft_go() {
-//     world().run("scenarios/interactor_nft.scen.json");
-// }
+#[test]
+#[ignore = "system SC not yet implemented"]
+fn interactor_nft_go() {
+    world().run("scenarios/interactor_nft.scen.json");
+}
 
-// #[test]
-// #[ignore = "system SC not yet implemented"]
-// fn interactor_nft_all_roles_go() {
-//     world().run("scenarios/interactor_nft_all_roles.scen.json");
-// }
+#[test]
+#[ignore = "system SC not yet implemented"]
+fn interactor_nft_all_roles_go() {
+    world().run("scenarios/interactor_nft_all_roles.scen.json");
+}
 
-// #[test]
-// fn interactor_wegld_go() {
-//     world().run("scenarios/interactor_wegld.scen.json");
-// }
+#[test]
+#[ignore = "not updated"]
+fn interactor_wegld_go() {
+    world().run("scenarios/interactor_wegld.scen.json");
+}
 
 #[test]
 fn remove_everyone_go() {
--- contracts/multisig/tests/multisig_scenario_rs_test.rs
+++ contracts/multisig/tests/multisig_scenario_rs_test.rs
@@ -81,20 +81,23 @@ fn deploy_duplicate_bm_rs() {
     world().run("scenarios/deploy_duplicate_bm.scen.json");
 }
 
-// #[test]
-// fn interactor_nft_rs() {
-//     world().run("scenarios/interactor_nft.scen.json");
-// }
+#[test]
+#[ignore = "not updated"]
+fn interactor_nft_rs() {
+    world().run("scenarios/interactor_nft.scen.json");
+}
 
-// #[test]
-// fn interactor_nft_all_roles_rs() {
-//     world().run("scenarios/interactor_nft_all_roles.scen.json");
-// }
+#[test]
+#[ignore = "not updated"]
+fn interactor_nft_all_roles_rs() {
+    world().run("scenarios/interactor_nft_all_roles.scen.json");
+}
 
-// #[test]
-// fn interactor_wegld_rs() {
-//     world().run("scenarios/interactor_wegld.scen.json");
-// }
+#[test]
+#[ignore = "not updated"]
+fn interactor_wegld_rs() {
+    world().run("scenarios/interactor_wegld.scen.json");
+}
 
 #[test]
 fn remove_everyone_rs() {
