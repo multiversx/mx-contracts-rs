231e425e37ca6ed5bc54a8a5779454dac8a3fa7d
Costin Carabaș <costin.carabas@gmail.com>
2024-08-22 17:54:52 +0300

    paymaster: forwardExecution interactor

--- contracts/paymaster/interactor/src/interactor_main.rs
+++ contracts/paymaster/interactor/src/interactor_main.rs
@@ -1,19 +1,31 @@
 #![allow(non_snake_case)]
 
+mod paymaster_config;
 mod proxy;
 
 use multiversx_sc_snippets::imports::*;
 use multiversx_sc_snippets::sdk;
+use paymaster_config::Config;
 use serde::{Deserialize, Serialize};
 use std::{
     io::{Read, Write},
     path::Path,
 };
 
-
 const GATEWAY: &str = sdk::gateway::DEVNET_GATEWAY;
 const STATE_FILE: &str = "state.toml";
 
+const ONE_UNIT: u64 = 1_000_000_000_000_000_000;
+const ONE_MILLION: u64 = 1_000_000;
+
+pub static WEGLD_TOKEN_ID: &[u8] = b"WEGLD-a28c59";
+pub static MEX_TOKEN_ID: &[u8] = b"MEX-a659d0";
+pub static ONE_TOKEN_ID: &[u8] = b"ONE-83a7c0";
+pub static USDC_TOKEN_ID: &[u8] = b"USDC-350c4e";
+pub static UTK_TOKEN_ID: &[u8] = b"UTK-14d57d";
+
+pub const SWAP_TOKENS_FIXED_INPUT_FUNC_NAME: &[u8] = b"swapTokensFixedInput";
+pub const SWAP_TOKENS_FIXED_OUTPUT_FUNC_NAME: &[u8] = b"swapTokensFixedOutput";
 
 #[tokio::main]
 async fn main() {
@@ -30,59 +42,59 @@ async fn main() {
     }
 }
 
-
 #[derive(Debug, Default, Serialize, Deserialize)]
 struct State {
-    contract_address: Option<Bech32Address>
+    contract_address: Option<Bech32Address>,
 }
 
 impl State {
-        // Deserializes state from file
-        pub fn load_state() -> Self {
-            if Path::new(STATE_FILE).exists() {
-                let mut file = std::fs::File::open(STATE_FILE).unwrap();
-                let mut content = String::new();
-                file.read_to_string(&mut content).unwrap();
-                toml::from_str(&content).unwrap()
-            } else {
-                Self::default()
-            }
-        }
-    
-        /// Sets the contract address
-        pub fn set_address(&mut self, address: Bech32Address) {
-            self.contract_address = Some(address);
-        }
-    
-        /// Returns the contract address
-        pub fn current_address(&self) -> &Bech32Address {
-            self.contract_address
-                .as_ref()
-                .expect("no known contract, deploy first")
+    // Deserializes state from file
+    pub fn load_state() -> Self {
+        if Path::new(STATE_FILE).exists() {
+            let mut file = std::fs::File::open(STATE_FILE).unwrap();
+            let mut content = String::new();
+            file.read_to_string(&mut content).unwrap();
+            toml::from_str(&content).unwrap()
+        } else {
+            Self::default()
         }
     }
-    
-    impl Drop for State {
-        // Serializes state to file
-        fn drop(&mut self) {
-            let mut file = std::fs::File::create(STATE_FILE).unwrap();
-            file.write_all(toml::to_string(self).unwrap().as_bytes())
-                .unwrap();
-        }
+
+    /// Sets the contract address
+    pub fn set_address(&mut self, address: Bech32Address) {
+        self.contract_address = Some(address);
     }
+
+    /// Returns the contract address
+    pub fn current_address(&self) -> &Bech32Address {
+        self.contract_address
+            .as_ref()
+            .expect("no known contract, deploy first")
+    }
+}
+
+impl Drop for State {
+    // Serializes state to file
+    fn drop(&mut self) {
+        let mut file = std::fs::File::create(STATE_FILE).unwrap();
+        file.write_all(toml::to_string(self).unwrap().as_bytes())
+            .unwrap();
+    }
+}
 
 struct ContractInteract {
     interactor: Interactor,
     wallet_address: Address,
     contract_code: BytesValue,
-    state: State
+    config: Config,
+    state: State,
 }
 
 impl ContractInteract {
     async fn new() -> Self {
         let mut interactor = Interactor::new(GATEWAY).await;
         let wallet_address = interactor.register_wallet(test_wallets::alice());
-        
+
         let contract_code = BytesValue::interpret_from(
             "mxsc:../output/paymaster.mxsc.json",
             &InterpreterContext::default(),
@@ -92,7 +104,8 @@ impl ContractInteract {
             interactor,
             wallet_address,
             contract_code,
-            state: State::load_state()
+            config: Config::load_config(),
+            state: State::load_state(),
         }
     }
 
@@ -110,21 +123,37 @@ impl ContractInteract {
             .run()
             .await;
         let new_address_bech32 = bech32::encode(&new_address);
-        self.state
-            .set_address(Bech32Address::from_bech32_string(new_address_bech32.clone()));
+        self.state.set_address(Bech32Address::from_bech32_string(
+            new_address_bech32.clone(),
+        ));
 
         println!("new address: {new_address_bech32}");
     }
 
     async fn forward_execution(&mut self) {
-        let token_id = String::new();
         let token_nonce = 0u64;
-        let token_amount = BigUint::<StaticApi>::from(0u128);
+        let token_amount = BigUint::<StaticApi>::from(ONE_UNIT) * ONE_MILLION;
 
-        let relayer_addr = bech32::decode("");
-        let dest = bech32::decode("");
-        let endpoint_name = ManagedBuffer::new_from_bytes(&b""[..]);
-        let endpoint_args = MultiValueVec::from(vec![ManagedBuffer::new_from_bytes(&b""[..])]);
+        let relayer_addr = &self.config.relayer_addr;
+
+        let dest = &self.config.egld_mex_pair_address;
+        let endpoint_name = ManagedBuffer::new_from_bytes(SWAP_TOKENS_FIXED_INPUT_FUNC_NAME);
+        let endpoint_args = MultiValueVec::from(vec![
+            ManagedBuffer::new_from_bytes(WEGLD_TOKEN_ID),
+            ManagedBuffer::new_from_bytes(b"1"),
+        ]);
+
+        let mut payments = ManagedVec::<StaticApi, EsdtTokenPayment<StaticApi>>::new();
+        payments.push(EsdtTokenPayment::new(
+            TokenIdentifier::from(WEGLD_TOKEN_ID),
+            token_nonce,
+            BigUint::<StaticApi>::from(ONE_UNIT / 100), // 0.01 WEGLD
+        ));
+        payments.push(EsdtTokenPayment::new(
+            TokenIdentifier::from(MEX_TOKEN_ID),
+            token_nonce,
+            token_amount, // 1_000_000 MEX
+        ));
 
         let response = self
             .interactor
@@ -134,7 +163,7 @@ impl ContractInteract {
             .gas(30_000_000u64)
             .typed(proxy::PaymasterContractProxy)
             .forward_execution(relayer_addr, dest, endpoint_name, endpoint_args)
-            .payment((TokenIdentifier::from(token_id.as_str()), token_nonce, token_amount))
+            .payment(payments)
             .returns(ReturnsResultUnmanaged)
             .prepare_async()
             .run()
@@ -142,5 +171,4 @@ impl ContractInteract {
 
         println!("Result: {response:?}");
     }
-
 }
