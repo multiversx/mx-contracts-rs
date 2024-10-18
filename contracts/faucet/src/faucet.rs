a1e1f79d02071cbe72e1280d40d680a11406c855
dragosrebegea <dragos.rebegea@multiversx.com>
2024-10-18 22:34:11 +0300

    add protection

--- contracts/faucet/src/faucet.rs
+++ contracts/faucet/src/faucet.rs
@@ -3,11 +3,17 @@
 #[allow(unused_imports)]
 use multiversx_sc::{derive_imports::*, imports::*};
 
+pub type Signature<M> = ManagedByteArray<M, ED25519_SIGNATURE_BYTE_LEN>;
+pub const ED25519_SIGNATURE_BYTE_LEN: usize = 64;
+
 /// An empty contract. To be used as a template when starting a new contract from scratch.
 #[multiversx_sc::contract]
 pub trait Faucet {
     #[init]
     fn init(&self) {}
+
+    #[upgrade]
+    fn upgrade(&self) {}
 
     #[only_owner]
     #[payable("*")]
@@ -21,7 +27,14 @@ pub trait Faucet {
     }
 
     #[endpoint(claim)]
-    fn claim(&self) {
+    fn claim(&self, signature: &Signature<Self::Api>) {
+        let caller = self.blockchain().get_caller();
+        let claimers_mapper = self.claimers();
+        require!(claimers_mapper.contains(&caller) == false, "Caller already claimed");
+
+        self.verify_signature(&caller, signature);
+        self.claimers().add(&caller);
+
         let mut payments = ManagedVec::new();
         for payment in self.payments().iter() {
             payments.push(payment);
@@ -30,9 +43,34 @@ pub trait Faucet {
         self.tx().to(ToCaller).with_multi_token_transfer(payments).transfer();
     }
 
+    fn verify_signature(
+        &self, 
+        caller: &ManagedAddress<Self::Api>,
+        signature: &Signature<Self::Api>,
+    ) {
+        let mut data = ManagedBuffer::new();
+        let _ = caller.dep_encode(&mut data);
+
+        let signer = self.signer().get();
+        self.crypto().verify_ed25519(
+            signer.as_managed_buffer(),
+            &data,
+            signature.as_managed_buffer(),
+        );
+    }
+
+    #[only_owner]
+    #[endpoint(setSigner)]
+    fn set_signer(&self, signer: ManagedAddress<Self::Api>) {
+        self.signer().set(&signer);
+    }
+
     #[storage_mapper("payments")]
     fn payments(&self) -> VecMapper<EsdtTokenPayment<Self::Api>>;
 
-    #[upgrade]
-    fn upgrade(&self) {}
+    #[storage_mapper("signer")]
+    fn signer(&self) -> SingleValueMapper<ManagedAddress<Self::Api>>;
+
+    #[storage_mapper("claimers")]
+    fn claimers(&self) -> WhitelistMapper<ManagedAddress<Self::Api>>;
 }
