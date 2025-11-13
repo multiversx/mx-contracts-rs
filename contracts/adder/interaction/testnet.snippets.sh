86b5b198c27fc4b884c4af8e2d35d4aac238af95
BiancaIalangi <bianca.ialangi@multiversx.com>
2025-11-13 18:09:49 +0200

    update adder mxpy interaction

--- contracts/adder/interaction/testnet.snippets.sh
+++ contracts/adder/interaction/testnet.snippets.sh
@@ -2,9 +2,10 @@ ALICE="${USERS}/alice.pem"
 ADDRESS=$(mxpy data load --key=address-testnet)
 DEPLOY_TRANSACTION=$(mxpy data load --key=deployTransaction-testnet)
 PROXY=https://testnet-api.multiversx.com
+PROJECT="../output/adder.wasm"
 
 deploy() {
-    mxpy contract deploy --bytecode=${PROJECT} --recall-nonce --pem=${ALICE} --gas-limit=50000000 --arguments 0 --send --outfile="deploy-testnet.interaction.json" --proxy=${PROXY} --chain=T || return
+    mxpy --verbose contract deploy --bytecode=${PROJECT} --pem=${ALICE} --arguments 0 --send --outfile="deploy-testnet.interaction.json" --proxy=${PROXY} || return
 
     TRANSACTION=$(mxpy data parse --file="deploy-testnet.interaction.json" --expression="data['emittedTransactionHash']")
     ADDRESS=$(mxpy data parse --file="deploy-testnet.interaction.json" --expression="data['contractAddress']")
@@ -18,7 +19,7 @@ deploy() {
 
 add() {
     read -p "Enter number: " NUMBER
-    mxpy contract call ${ADDRESS} --recall-nonce --pem=${ALICE} --gas-limit=5000000 --function="add" --arguments ${NUMBER} --send --proxy=${PROXY} --chain=T
+    mxpy contract call ${ADDRESS} --pem=${ALICE} --function="add" --arguments ${NUMBER} --send --proxy=${PROXY}
 }
 
 getSum() {
